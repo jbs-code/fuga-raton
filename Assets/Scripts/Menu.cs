7bc58b583fd444712f6fb2291c92429ea217f3ac
Jaime BS <jaimejb.code@gmail.com>
2022-02-09 14:21:36 -0600

    Reparando parte del build

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -17,11 +17,11 @@ public class Menu : MonoBehaviour {
 	void OnGUI(){
 		GUI.skin = sk;
 
-        if (GUI.Button(new Rect(Screen.width / 1.4f, Screen.height / 1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Salir"))
-            Application.Quit();
-        if (GUI.Button (new Rect (Screen.width / 1.95f, Screen.height/1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Ayuda"))
+        //if (GUI.Button(new Rect(Screen.width / 1.4f, Screen.height / 1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Salir"))
+        //    Application.Quit();
+        if (GUI.Button (new Rect (Screen.width / 1.43f, Screen.height/1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Ayuda"))
 			 SceneManager.LoadScene("preferencias");
-		if (GUI.Button (new Rect (Screen.width / 3.3f, Screen.height/1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Puntuación"))
+		if (GUI.Button (new Rect (Screen.width / 2.5f, Screen.height/1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Puntuación"))
 			 SceneManager.LoadScene("score");
 		if (GUI.Button (new Rect (Screen.width / 10.0f, Screen.height/1.25f, Screen.width / 5.0f, Screen.width / 12.0f), "Jugar"))
 			 SceneManager.LoadScene("escena1");
